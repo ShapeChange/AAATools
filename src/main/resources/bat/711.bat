6d8f8b15e2187e00cb648dfbb42cc94f9e46b706
Johannes Echterhoff <echterhoff@interactive-instruments.de>
2022-09-05 16:11:16 +0200

    Update des bat-Skripts, Update der Dokumentation
    Signed-off-by: Johannes Echterhoff <echterhoff@interactive-instruments.de>

--- src/main/resources/bat/711.bat
+++ src/main/resources/bat/711.bat
@@ -1,3 +1,5 @@
+@echo off
+
 java -jar AAATools-${project.version}.jar -c "Konfigurationen/AAAKatalog-GV-1.0.0-SCXML.xml" 
 java -jar AAATools-${project.version}.jar -c "Konfigurationen/AAAKatalog-LB-1.0.1-SCXML.xml" 
 java -jar AAATools-${project.version}.jar -c "Konfigurationen/AAAKatalog-LB-1.0.1-Aenderungen-SCXML.xml"
@@ -13,3 +15,12 @@ java -jar AAATools-${project.version}.jar -c "Konfigurationen/NAS-LB-1.0.1.xml"
 java -jar AAATools-${project.version}.jar -c "Konfigurationen/NAS-LN-1.0.1.xml" 
 java -jar AAATools-${project.version}.jar -c "Konfigurationen/NAS-AK-1.0.0.xml" 
 java -jar AAATools-${project.version}.jar -c "Konfigurationen/NAS-7.1.1.xml"
+
+for /r "Ausgaben" %%F in (katalog?.adoc) do @(
+    @echo Generiere HTML aus %%~dpnxF   
+    call asciidoctor %%~dpnxF
+    @echo Generiere PDF aus %%~dpnxF
+    call asciidoctor-pdf -a pdf-themesdir=%%~dpF/resources/themes -a pdf-theme=basic %%~dpnxF
+)
+
+PAUSE
